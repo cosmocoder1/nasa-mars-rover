cf9ebae534ba8a12351880d7f7b30478b294f71a
Nathan Lucy <cosmocoder1@gmail.com>
2020-11-02 19:18:01 -0600

    tabs functional, API functional, photo display in progress

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,7 +1,9 @@
+
+
 let store = {
-    user: { name: "Student" },
-    apod: '',
+    data: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    currentTab: '',
 }
 
 // add our markup to the page
@@ -9,7 +11,7 @@ const root = document.getElementById('root')
 
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState)
-    render(root, store)
+    
 }
 
 const render = async (root, state) => {
@@ -17,32 +19,104 @@ const render = async (root, state) => {
 }
 
 
+  const content = () => {
+    return `
+    <header>
+      <div id="navContainer">
+        <div class="navBar" id="navBar">
+          <div class="tabButton"><a href="#" id="curiosity" onclick="displayCuriosityTab()">Curiosity rover</a></div>
+          <div class="tabButton"><a href="#" id="opportunity" onclick="displayOpportunityTab()">Opportunity rover</a></div>
+          <div class="tabButton"><a href="#" id="spirit" onclick="displaySpiritTab()">Spirit rover</a></div>
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <div id="tabContainer">
+        <div class="tab" id="curiosityTab">Curiosity tab</div>
+        <div class="tab" id="opportunityTab">Opportunity tab</div>
+        <div class="tab" id="spiritTab">Spirit tab</div>
+      </div>
+    </main>
+
+    <footer></footer>
+`
+  }
+
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Test</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
+    
+  return content();
 }
 
+    // define buttons and tabs
+const curiosityButton = document.getElementById('curiosity');
+const opportunityButton = document.getElementById('opportunity');
+const spiritButton = document.getElementById('spirit');
+
+/*
+getData(data, 'curiosity');
+getData(data, 'opportunity');
+getData(data, 'spirit');
+*/
+
+//display tab function
+
+const displayCuriosityTab = () => {
+    
+    let { data } = store;
+    const curiosityTab = document.getElementById('curiosityTab');
+    spiritTab.style.display = 'none';
+    opportunityTab.style.display = 'none';
+    curiosityTab.style.display = 'inline-block';
+    
+    getData(data, 'curiosity');
+    
+ /*
+    photo = document.createElement('div');
+    photo.innerHTML = data.photos[0];
+    curiosityTab.appendChild(photo);
+  */
+}
+
+
+/*  TEST UNIT
+const showStore = () => {
+        console.log(store.data);
+    }
+    
+    setTimeout(showStore, 3000)
+*/    
+
+const displayOpportunityTab = () => {
+    let { rovers, data } = store;
+    const opportunityTab = document.getElementById('opportunityTab');
+    spiritTab.style.display = 'none';
+    curiosityTab.style.display = 'none';
+    opportunityTab.style.display = 'inline-block';
+
+
+}
+
+
+const displaySpiritTab = () => {
+    let { rovers, data } = store;
+    const spiritTab = document.getElementById('spiritTab');
+    curiosityTab.style.display = 'none';
+    opportunityTab.style.display = 'none';
+    spiritTab.style.display = 'inline-block';
+
+
+}
+
+
+/*  TEST API UNIT
+async function testApiPull() {
+    const apiFetch = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=1000&api_key=Gv5rFsnXyiFh4tuFN5Bwypvc1HKxAWUCvxZeBtnY').then((res) => res.json());
+    console.log(apiFetch);
+}
+testApiPull();
+*/
 
 
 // listening for load event because page should load before any JS is called
@@ -50,58 +124,26 @@ window.addEventListener('load', () => {
     render(root, store)
 })
 
+
+
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
 
-    return `
-        <h1>Hello!</h1>
-    `
-}
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
-
-// ------------------------------------------------------  API CALLS
 
 // Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
-    fetch(`http://localhost:3000/apod`)
+const getData = (state, currentRover) => {
+    let { data } = state;
+    
+    let result = fetch(`http://localhost:3000/data?rover=${currentRover}`, {
+        headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+    })
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+        .then(data => updateStore(store, { data }))
+
 
     return data
 }
