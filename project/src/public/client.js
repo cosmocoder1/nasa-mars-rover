c40e9efaa9b4f67a1568b929be0857d47ef30b04
Nathan Lucy <cosmocoder1@gmail.com>
2020-11-05 11:04:15 -0600

    final commit

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,3 +1,4 @@
+// define store
 let store = {
     data: '',
     rovers: Immutable.List(['Curiosity', 'Opportunity', 'Spirit']),
@@ -32,6 +33,9 @@ const content = () => {
     </header>
 
     <main>
+    <div id="welcomeContainer">
+      <div id="welcome">Welcome!  Please select a Rover.</div>
+    </div>  
       <div id="tabContainer">
       </div>
     </main>
@@ -41,17 +45,16 @@ const content = () => {
   }
 
 
-// create content
+// create content - higher order
 const App = (state) => {
     
   return content();
 }
 
-    // define buttons and tabs
+// define buttons and tabs
 const curiosityButton = document.getElementById('curiosity');
 const opportunityButton = document.getElementById('opportunity');
 const spiritButton = document.getElementById('spirit');
-
 
 
 //display tab function
@@ -59,14 +62,16 @@ const spiritButton = document.getElementById('spirit');
 const displayCuriosityTab = () => {
     
     let { data } = store;
+    const welcomeContainer = document.getElementById('welcomeContainer');
+    welcomeContainer.style.display = 'none';
     const tabContainer = document.getElementById('tabContainer');
     tabContainer.removeChild(tabContainer.firstChild);
     const curiosityTab = document.createElement('div');
     curiosityTab.classList.add("tab");
     curiosityTab.setAttribute("id", "curiosityTab");
     tabContainer.appendChild(curiosityTab);
-    
-    getData(data, 'Curiosity', curiosityTab, 'active', 'November 26, 2011', 'August 6, 2012');
+    // call function to retrieve and format tab data
+    getData(data, curiosityTab, 'Curiosity', 'active', 'November 26, 2011', 'August 6, 2012');
 
     }
 
@@ -74,28 +79,32 @@ const displayCuriosityTab = () => {
 const displayOpportunityTab = () => {
 
     let { data } = store;
+    const welcomeContainer = document.getElementById('welcomeContainer');
+    welcomeContainer.style.display = 'none';
     const tabContainer = document.getElementById('tabContainer');
     tabContainer.removeChild(tabContainer.firstChild);
     const opportunityTab = document.createElement('div');
     opportunityTab.classList.add("tab");
     opportunityTab.setAttribute("id", "opportunityTab");
     tabContainer.appendChild(opportunityTab);
-
-    getData(data, 'Opportunity', opportunityTab, 'complete', 'July 7, 2003', 'January 25, 2004');
+    // call function to retrieve and format tab data
+    getData(data, opportunityTab, 'Opportunity', 'complete', 'July 7, 2003', 'January 25, 2004');
 
 }
 
 
 const displaySpiritTab = () => {
     let { data } = store;
+    const welcomeContainer = document.getElementById('welcomeContainer');
+    welcomeContainer.style.display = 'none';
     const tabContainer = document.getElementById('tabContainer');
     tabContainer.removeChild(tabContainer.firstChild);
     const spiritTab = document.createElement('div');
     spiritTab.classList.add("tab");
     spiritTab.setAttribute("id", "spiritTab");
     tabContainer.appendChild(spiritTab);
-
-    getData(data, 'Spirit', spiritTab, 'complete', 'June 10, 2003', 'January 1, 2004');
+    // call function to retrieve and format tab data
+    getData(data, spiritTab, 'Spirit', 'complete', 'June 10, 2003', 'January 1, 2004');
     
   }
 
@@ -106,22 +115,17 @@ window.addEventListener('load', () => {
     render(root, store)
 })
 
-
-
 // ------------------------------------------------------  COMPONENTS
-
-
-
 
 // API call from localhost
 
-const getData = async (state, rover, tab, status, dateLaunched, dateLanded) => {
+const getData = async (state, tab, rover, status, dateLaunched, dateLanded) => {
   
     let { data } = state;
     let result = await fetch(`http://localhost:3000/data?rover=${rover.toLowerCase()}`)
         .then(res => res.json())
         .then(data => updateStore(store, { data }))
-        console.dir(store.data);
+      
 
         // add rover data to tab
         dataContainer = document.createElement('div');
@@ -143,26 +147,20 @@ const getData = async (state, rover, tab, status, dateLaunched, dateLanded) => {
         missionStatus.classList.add('data');
         missionStatus.innerHTML = `Mission status: ${status}`;
 
-        photoDate = document.createElement('div');
-        photoDate.classList.add('data');
-        /*
-        photoDate.innerHTML = `photo date`;
-        */
-
+        
         // display data
         tab.appendChild(dataContainer);
         dataContainer.appendChild(roverName);
         dataContainer.appendChild(launchDate);
         dataContainer.appendChild(landDate);
         dataContainer.appendChild(missionStatus);
-        dataContainer.appendChild(photoDate);
+      
 
         // display photos
         photoContainer = document.createElement('div');
         photoContainer.classList.add('photoContainer');
         tab.appendChild(photoContainer);
 
-        
         for (let i = 0; i < store.data.data.latest_photos.length && i < 5; i++) {
         photo = document.createElement('img');
         photoUrlArr = store.data.data.latest_photos.map(input => input.img_src);
@@ -170,16 +168,6 @@ const getData = async (state, rover, tab, status, dateLaunched, dateLanded) => {
         photoContainer.appendChild(photo);
         }
         
-
         return result
 
         }
-
-    
-/*
-async function testApiPull() {
-    const apiFetch = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=Gv5rFsnXyiFh4tuFN5Bwypvc1HKxAWUCvxZeBtnY').then((res) => res.json());
-    console.log(apiFetch);
-}
-testApiPull();
-*/
